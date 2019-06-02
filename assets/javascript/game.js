014df128d805892bb02aaa7022a8c8aa897e2fe0
Adam Minkus <adam.minkus@gmail..com>
2019-06-02 13:20:10 -0700

    event listener working and populating answer on html, still need to figure out how to accoutn for duplicate letters in answer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,52 @@
 
-var words = ["Jerry", "George", "Kramer", "Newman", "Elaine", "Frank", "Morty"];
-
-var word = words[Math.floor(Math.random() * words.length)];
-
-var answerArray = [];
-
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+var words = ["jerry", "george", "kramer", "newman", "elaine", "frank", "morty"];
 
 var guessedLetters = [];
 var wins = 0;
 var guessesRemaining = 10
-
-
+var answerArray=[];
+var word = words[Math.floor(Math.random() * words.length)];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_ ";
+}
 
 
 
 var winsText = document.getElementById("wins-text");
-var currentWordText = document.getElementById("currentWord-text");
-var guessesRemaining = document.getElementById("guessesRemaining-text");
-var chosenLetters = document.getElementById("chosenLetters-text");
+var guessesRemainingText = document.getElementById("guessesRemaining-text");
+var guessedLettersText = document.getElementById("guessedLetters-text");
+var answerArrayText = document.getElementById("answer-text")
+
 
 
 
 document.onkeyup = function(event) {
 
     
-    var userGuess= event.key;
-        
-        
+    var userGuess = event.key.toLowerCase();
+
+    var matchingLetter = word.indexOf(userGuess);
+ 
+
+
+    if (word.indexOf(userGuess) >= 0) {
+        answerArray[matchingLetter] = userGuess;
     }
+  
+   
+   
+        
+
+        winsText.textContent = wins;
+        guessesRemainingText.textContent = guessesRemaining;
+        guessedLettersText.textContent = guessedLetters;
+        answerArrayText.textContent = answerArray.join(" ");
+
+    };
+
+    
 
 
+
+    
 

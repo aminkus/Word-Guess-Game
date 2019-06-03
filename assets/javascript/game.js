e653bb2a355aa5c736cb80646c4fa425e1e8b62a
Adam Minkus <adam.minkus@gmail..com>
2019-06-03 09:19:03 -0700

    updated logic in js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,20 @@
-
-var words = ["jerry", "george", "kramer", "newman", "elaine", "frank", "morty"];
-
+//create array of words to be guessed by user
+var words = ["jerry"];
+//create variables to store wins, losses, guessed letters, and answer
 var guessedLetters = [];
 var wins = 0;
 var guessesRemaining = 10
 var answerArray=[];
+
+//create loop that puts an underscore in place of each letter 
 var word = words[Math.floor(Math.random() * words.length)];
+
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_ ";
 }
-function reset () {
-    var wins = 0;
-    word;
-    guessesRemaining = 10;
-    answerArray = 0;
-}
 
 
+//creates reference point for display to HTML
 var winsText = document.getElementById("wins-text");
 var guessesRemainingText = document.getElementById("guessesRemaining-text");
 var guessedLettersText = document.getElementById("guessedLetters-text");
@@ -27,28 +25,26 @@ var answerArrayText = document.getElementById("answer-text")
 
 document.onkeyup = function(event) {
 
-    
+    //create event listener
     var userGuess = event.key.toLowerCase();
-
-    var matchingLetter = word.indexOf(userGuess);
- 
+    
 
 
-if (guessesRemaining > 0) {
-
-  
-    if (word.indexOf(userGuess) >= 0) {
-        answerArray[matchingLetter] = userGuess;
+for(var i = 0; i < word.length; i++) {
+    if (word[i] === userGuess) {
+        answerArray[i] = userGuess;
     }
-    else if 
-        (word.indexOf(userGuess == -1)) { 
+}
+     if 
+        (word.indexOf(userGuess) == -1) { 
             guessesRemaining--;
         }
+    
 
-    } else {
-        alert ("YOU LOSE!")
-    }
 
+
+
+  
 
 
 
